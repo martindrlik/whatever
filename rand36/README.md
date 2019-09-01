b00fae371a1cf72002207583ea92164b75de62c7
Martin Drlík <martindrlik@hotmail.com>
2019-09-01 13:06:37 +0200

    rand36: initial

---
+++ rand36/README.md
@@ -0,0 +1,11 @@
+# rand36
+
+Command rand36 writes random (given by source) word of specified length.
+
+## Example Usage
+
+```
+	$ /path/to/rand36 -source /dev/random -base 2
+	0011000100
+```
+
---
+++ rand36/rand36.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"math/big"
+	"os"
+	"strconv"
+)
+
+var (
+	base   = flag.Int("base", 36, "base, for 2 <= base <= 36; uses the lower-case letters 'a' to 'z' for digit values >= 10")
+	length = flag.Int("length", 10, "lenght of output word")
+	source = flag.String("source", "", "source file for rand")
+)
+
+func main() {
+	flag.Parse()
+	r, err := os.Open(*source)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not source file: %v\n", err)
+		return
+	}
+	defer r.Close()
+	for i := 0; i < *length; i++ {
+		n, err := rand.Int(r, big.NewInt(int64(*base)))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "rand.Int: %v\n", err)
+			break
+		}
+		fmt.Print(strconv.FormatInt(n.Int64(), *base))
+	}
+	fmt.Println()
+}
